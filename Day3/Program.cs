f72c7eedb0dc748cb4904433be7f32e1f75ed151
Barry Wheeler <wheelerbarry+github@hotmail.com>
2024-12-03 08:40:06 +0000

    Part2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -6,20 +6,34 @@ var matches = new List<string>();
 
 foreach (var line in input)
 {
-    // MAy need to check > 4 digit numbers.
-    matches.AddRange(Regex.Matches(line, "mul\\(\\d*,\\d*\\)").Select(m => m.Value));
+    //matches.AddRange(Regex.Matches(line, "mul\\(\\d*,\\d*\\)").Select(m => m.Value));
+    matches.AddRange(Regex.Matches(line, "mul\\(\\d*,\\d*\\)|do\\(\\)|don\'t\\(\\)").Select(m => m.Value));
 }
 
 var total = 0;
+var ignore = false;
 
 foreach (var match in matches)
 {
-    var parts = match.Replace("mul(", string.Empty).Replace(")", string.Empty).Split(',').Select(s => int.Parse(s)).ToArray();
-    var result = parts[0] * parts[1];
+    if (match == "don't()")
+    {
+        ignore = true;
+    }
 
-    Console.WriteLine($"{match} = {result}");
+    if (match == "do()")
+    {
+        ignore = false;
+    }
 
-    total += result;
+    if (!ignore && match.StartsWith("mul"))
+    {
+        var parts = match.Replace("mul(", string.Empty).Replace(")", string.Empty).Split(',').Select(s => int.Parse(s)).ToArray();
+        var result = parts[0] * parts[1];
+
+        Console.WriteLine($"{match} = {result}");
+
+        total += result;
+    }
 }
 
 Console.WriteLine($"Total = {total}");
