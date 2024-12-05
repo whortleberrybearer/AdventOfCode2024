dfba54d9302d0d12d68c043a441ed8643c7d8699
Barry Wheeler <wheelerbarry+github@hotmail.com>
2024-12-05 08:46:48 +0000

    Part 1

---
+++ Day5/Program.cs
@@ -0,0 +1,31 @@
+﻿var input = File.ReadAllLines("Input.txt");
+
+var rules = input.TakeWhile(i => i != string.Empty).Select(i => i.Split('|').Select(r => int.Parse(r)).ToArray()).ToArray();
+var orders = input.Skip(rules.Length + 1).Select(i => i.Split(',').Select(o => int.Parse(o)).ToArray()).ToArray();
+
+var total = 0;
+var validOrders = new List<int[]>();
+
+foreach (var order in orders)
+{
+    var invalid = false;
+
+    for (var i = 0; i < order.Length; i++)
+    {
+        var orderRules = rules.Where(r => r[0] == order[i]).ToArray();
+        var invalidPreviousPages = orderRules.Select(or => or[1]).ToArray();
+
+        if (order.Take(i).Intersect(invalidPreviousPages).Any())
+        {
+            invalid = true;
+            break;
+        }
+    }
+
+    if (!invalid)
+    {
+        total += order[(order.Length / 2)];
+    }
+}
+
+Console.WriteLine($"Total = {total}");
