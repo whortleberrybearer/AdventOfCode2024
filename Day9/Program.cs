c3edc7404025e717ffa1df5e65adbfa4e9c3d3bc
Barry Wheeler <wheelerbarry+github@hotmail.com>
2024-12-09 14:32:25 +0000

    Part 2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -2,6 +2,7 @@
 
 var input = File.ReadAllLines("Input.txt")[0];
 
+var blocks = new List<Block>();
 var fullString = new List<string>();
 var fileId = 0;
 
@@ -10,12 +11,14 @@ for (var i = 0; i < input.Length; i++)
     if (i % 2 == 0)
     {
         fullString.AddRange(Enumerable.Repeat(fileId.ToString(), int.Parse(input[i].ToString())));
+        blocks.Add(new Block() { FileId = fileId, Length = int.Parse(input[i].ToString()) });
 
         fileId += 1;
     }
     else
     {
         fullString.AddRange(Enumerable.Repeat(".", int.Parse(input[i].ToString())));
+        blocks.Add(new Block() { FileId = -1, Length = int.Parse(input[i].ToString()) });
     }
 }
 
@@ -26,19 +29,57 @@ foreach (var s in fullString)
 
 Console.WriteLine();
 
-var replaceAt = fullString.IndexOf(".");
+//var replaceAt = fullString.IndexOf(".");
 
-while (replaceAt != -1)
+//while (replaceAt != -1)
+//{
+//    var toMove = fullString.Last();
+//    fullString.RemoveAt(fullString.Count - 1);
+
+//    if (toMove != ".")
+//    {
+//        fullString[replaceAt] = toMove;
+
+//        replaceAt = fullString.IndexOf(".");
+//    }
+//}
+
+for (var i = blocks.Count - 1; i >= 0; i--)
 {
-    var toMove = fullString.Last();
-    fullString.RemoveAt(fullString.Count - 1);
+    var blockToMove = blocks[i];
 
-    if (toMove != ".")
+    if (blockToMove.Moved)
     {
-        fullString[replaceAt] = toMove;
-
-        replaceAt = fullString.IndexOf(".");
+        continue;
     }
+
+    if (blockToMove.FileId != -1)
+    {
+        var spaceToInsert = blocks.FirstOrDefault(b => b.FileId == -1 && b.Length >= blockToMove.Length);
+
+        if (spaceToInsert is not null)
+        {
+            var positionToInsert = blocks.IndexOf(spaceToInsert);
+
+            if (positionToInsert > i)
+            {
+                continue;
+            }
+
+            blocks.Insert(positionToInsert, new Block() { FileId = blockToMove.FileId, Length = blockToMove.Length, Moved = true });
+
+            blockToMove.FileId = -1;
+
+            spaceToInsert.Length -= blockToMove.Length;
+        }
+    }
+}
+
+fullString = new List<string>();
+
+foreach (var block in blocks)
+{
+    fullString.AddRange(Enumerable.Repeat(block.FileId == -1 ? "." : block.FileId.ToString(), block.Length));
 }
 
 foreach (var s in fullString)
@@ -48,14 +89,24 @@ foreach (var s in fullString)
 
 Console.WriteLine();
 
-var total = 0l;
+var total = 0L;
 
 for (var i = 0; i < fullString.Count; i++)
 {
-    total += (i * int.Parse(fullString[i]));
+    if (fullString[i] != ".")
+    {
+        total += (i * int.Parse(fullString[i]));
+    }
 }
 
 Console.WriteLine();
 Console.WriteLine($"Total = {total}");
 
-// 2132861615 too low
+class Block
+{
+    public int FileId { get; set; }
+
+    public int Length { get; set; }
+
+    public bool Moved { get; set; } = false;
+}
