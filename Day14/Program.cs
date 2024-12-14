ba18dd61a0d33c2612adf12fc50d9ca72a3c4756
Barry Wheeler <wheelerbarry+github@hotmail.com>
2024-12-14 19:32:41 +0000

    Corrected merge

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -31,19 +31,19 @@ var second = 0;
 
 for (; second < 10000; second++)
 {
-foreach (var robot in robots)
-{
+    foreach (var robot in robots)
+    {
         robot.MultipleMove(1, maxRow, maxCol);
     }
 
     var noSiblings = 0;
     var j = 0;
-    
+
     for (; j < robots.Count; j++)
     {
-        var sibling = robots.FirstOrDefault(r => 
-            (r != robots[j]) && 
-            (r.Posiition.X >= robots[j].Posiition.X - 1) && 
+        var sibling = robots.FirstOrDefault(r =>
+            (r != robots[j]) &&
+            (r.Posiition.X >= robots[j].Posiition.X - 1) &&
             (r.Posiition.X <= robots[j].Posiition.X + 1) &&
             (r.Posiition.Y >= robots[j].Posiition.Y + 1) &&
             (r.Posiition.Y <= robots[j].Posiition.Y + 1));
@@ -57,34 +57,58 @@ foreach (var robot in robots)
                 break;
             }
         }
-}
+    }
 
     Console.WriteLine($"Pass: {second}, Checks: {j}");
 
     if (noSiblings < requiredSiblings)
     {
-for (var row = 0; row < maxRow; row++)
-{
-    for (var col = 0; col < maxCol; col++)
-    {
-        var count = robots.Count(r => r.Posiition.X == col && r.Posiition.Y == row);
+        for (var row = 0; row < maxRow; row++)
+        {
+            for (var col = 0; col < maxCol; col++)
+            {
+                var count = robots.Count(r => r.Posiition.X == col && r.Posiition.Y == row);
 
-        Console.Write(count == 0 ? "." : count.ToString());
+                Console.Write(count == 0 ? "." : count.ToString());
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        break;
     }
-
-    Console.WriteLine();
 }
 
-Console.WriteLine();
-Console.WriteLine();
+//var xDiv = Math.DivRem(maxCol, 2);
+//var yDiv = Math.DivRem(maxRow, 2);
 
+//var rightQuadrantStart = xDiv.Quotient;
 
+//if (xDiv.Remainder != 0)
+//{
+//    rightQuadrantStart += 1;
+//}
 
+//var bottomQuadrantStart = yDiv.Quotient;
 
+//if (yDiv.Remainder != 0)
+//{
+//    bottomQuadrantStart += 1;
+//}
 
+//var topLeft = robots.Count(r => r.Posiition.X < xDiv.Quotient && r.Posiition.Y < yDiv.Quotient);
+//var topRight = robots.Count(r => r.Posiition.X >= rightQuadrantStart && r.Posiition.Y < yDiv.Quotient);
+//var bottomLeft = robots.Count(r => r.Posiition.X < xDiv.Quotient && r.Posiition.Y >= bottomQuadrantStart);
+//var bottomRight = robots.Count(r => r.Posiition.X >= rightQuadrantStart && r.Posiition.Y >= bottomQuadrantStart);
 
+//Console.WriteLine($"TL={topLeft}, TR={topRight}, BL={bottomLeft}, BR={bottomRight}");
 
+//var total = topLeft * topRight * bottomLeft * bottomRight;
 
+var total = second + 1;
 
 Console.WriteLine($"Total = {total}");
 
