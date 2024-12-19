63f6c543ca3f1b89aed7d4d686d26c10a2298560
Barry Wheeler <wheelerbarry+github@hotmail.com>
2024-12-19 11:03:18 +0000

    Part 1

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,15 +1,16 @@
-﻿
-
-var input = File.ReadAllLines("Input.txt");
+﻿var input = File.ReadAllLines("Input.txt");
 var patterns = input[0].Split(", ");
 var towels = input.Skip(2).ToArray();
 var invalid = 0;
+var expansions = new Dictionary<string, List<Expansion>>();
 
 foreach (var towel in towels)
 {
     var expandedOptions = ExpandOptions(towel);
 
-    if (!CanMake(expandedOptions))
+    Console.WriteLine($"{towel} has {expandedOptions.Count} expansions");
+
+    if (!expandedOptions.Any())
     {
         Console.WriteLine($"{towel} not valid");
 
@@ -21,92 +22,48 @@ var total = towels.Length - invalid;
 
 Console.WriteLine($"Total = {total}");
 
-List<string[]> ExpandOptions(string towel)
+List<Expansion> ExpandOptions(string towel)
 {
-    var options = new List<string[]>();
+    Console.WriteLine($"Explanding {towel}");
+
+    var options = new List<Expansion>();
 
     for (var length = 1; length <= towel.Length; length++)
     {
         var start = towel.Substring(0, length);
 
-        if (length < towel.Length)
+        // Only continue expanding is already got a valid start pattern.
+        if (patterns.Contains(start))
         {
-            var endOptions = ExpandOptions(towel.Substring(length));
-
-            foreach (var option in endOptions)
+            if (length < towel.Length)
             {
-                options.Add(option.Prepend(start).ToArray());
+                var toExpand = towel.Substring(length);
+
+                if (!expansions.TryGetValue(toExpand, out var endOptions))
+                {
+                    endOptions = ExpandOptions(toExpand);
+
+                    expansions[toExpand] = endOptions;
+                }
+
+                if (endOptions.Any())
+                {
+                    options.Add(new Expansion() { Start = start, End = endOptions });
+                }
             }
-        }
-        else
-        {
-            options.Add(new string[] { start });
+            else
+            {
+                options.Add(new Expansion() { Start = start, End = new List<Expansion>() });
+            }
         }
     }
 
     return options;
 }
 
-bool CanMake(List<string[]> expandedOptions)
+class Expansion
 {
-    foreach (var option in expandedOptions)
-    {
-        var valid = true;
+    public string Start { get; init; }
 
-        foreach (var towel in option)
-        {
-            if (!patterns.Contains(towel))
-            {
-                valid = false;
-                break;
-            }
-        }
-
-        if (valid)
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-
-public class StringPermuter
-{
-    private string _word;
-
-    public StringPermuter(string word)
-    {
-        _word = word;
-    }
-
-    public void Permute()
-    {
-        Console.WriteLine($"Permutations of {_word}");
-        Process(_word.Length);
-    }
-
-    private void Process(int c)
-    {
-        if (c == 1)
-        {
-            Console.WriteLine(_word);
-            return;
-        }
-
-        for (var i = 0; i < c; i++)
-        {
-            Process(c - 1);
-            Rotate(c);
-        };
-    }
-
-    private void Rotate(int c)
-    {
-        var target = _word.Length - c;
-        _word = _word.Substring(0, target)
-            + _word.Substring(target + 1)
-            + _word.Substring(target, 1);
-    }
+    public List<Expansion> End { get; init; }
 }
