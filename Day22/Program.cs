e6d9b3ac81b5386179b1ee2f8c78868e3fef2bdd
Barry Wheeler <wheelerbarry+github@hotmail.com>
2024-12-22 10:48:17 +0000

    Hardcoded sample

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -1,22 +1,42 @@
-﻿var input = File.ReadAllLines("Input.txt");
+﻿
 
-var total = 0L;
+var input = File.ReadAllLines("Input.txt");
+
+//var total = 0L;
+var buyerOptions = new List<BuyerOption>();
 
 foreach (var line in input)
 {
     var secretNumber = long.Parse(line);
+    var buyerOption = new BuyerOption();
 
+    buyerOptions.Add(buyerOption);
+
+    var item = CalcaulateItem(secretNumber, null);
+
+    buyerOption.Items.Add(item);
+
+    var previosBananas = item.Bananas;
+    
     for (var i = 0; i < 2000; i++)
     {
         secretNumber = Part1(secretNumber);
         secretNumber = Part2(secretNumber);
         secretNumber = Part3(secretNumber);
 
-        Console.WriteLine($"Calc {i + 1} = {secretNumber}");
+        //Console.WriteLine($"Calc {i + 1} = {secretNumber}");
+
+        item = CalcaulateItem(secretNumber, previosBananas);
+
+        buyerOption.Items.Add(item);
+
+        previosBananas = item.Bananas;
     }
 
-    total += secretNumber;
+    //total += secretNumber;
 }
+
+var total = FindBestSequence();
 
 Console.WriteLine($"Total = {total}");
 
@@ -49,4 +69,54 @@ long Mix(long secretNumber, long mixNumber)
 long Prune(long secretNumber)
 {
     return secretNumber % 16777216;
+}
+
+Item CalcaulateItem(long secretNumber, long? previosBananas)
+{
+    var bananas = secretNumber % 10;
+    var item = new Item() { Bananas = bananas, Change = bananas - previosBananas };
+
+    Console.WriteLine($"{secretNumber} = {item.Bananas} ({item.Change})");
+
+    return item;
+}
+
+long FindBestSequence()
+{
+    var total = 0L;
+
+    foreach (var buyerOption in buyerOptions)
+    {
+        total += buyerOption.FindSewuenceBananas(new int[] { -2, 1, -1, 3 });
+    }
+
+    return total;
+}
+
+class Item
+{
+    public long Bananas { get; init; }
+
+    public long? Change { get; init; }
+}
+
+class BuyerOption
+{
+    public List<Item> Items { get; } = new List<Item>();
+
+    public long FindSewuenceBananas(int[] sequence)
+    {
+        for (var i = 0; i < Items.Count; i++)
+        {
+            if (Items[i].Change == sequence[0] &&
+                Items[i + 1].Change == sequence[1] &&
+                Items[i + 2].Change == sequence[2] &&
+                Items[i + 3].Change == sequence[3])
+            {
+                return Items[i + 3].Bananas;
+            }
+        }
+
+        return 0;
+    }
 }
