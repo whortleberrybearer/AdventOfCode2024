7804cd3b263693598def2031a3881de978b475c2
Barry Wheeler <wheelerbarry+github@hotmail.com>
2024-12-23 09:29:04 +0000

    Samepe works

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -20,27 +20,62 @@ foreach (var line in input)
     }
 }
 
-var options = new List<string>();
+//var options = new List<string>();
+
+//foreach (var connection in clientConnections)
+//{
+//    foreach (var connection1 in connection.Value.Where(c => c != connection.Key))
+//    {
+//        foreach (var connection2 in clientConnections[connection1].Where(c => c != connection.Key && c != connection1))
+//        {
+//            if (clientConnections[connection2].Contains(connection.Key))
+//            {
+//                var group = string.Join(",", new string[] { connection.Key, connection1, connection2 }.Order());
+
+//                if (!options.Contains(group))
+//                {
+//                    options.Add(group);
+//                }
+//            }
+//        }
+//    }
+//}
+
+//var total = options.Count(o => o.StartsWith("t") || o.Contains(",t"));
+
+var groups = new List<List<string>>();
+
+foreach (var key in clientConnections.Keys)
+{
+    groups.Add(new List<string>() { key });
+}
 
 foreach (var connection in clientConnections)
 {
-    foreach (var connection1 in connection.Value.Where(c => c != connection.Key))
+    foreach (var group in groups)
     {
-        foreach (var connection2 in clientConnections[connection1].Where(c => c != connection.Key && c != connection1))
-        {
-            if (clientConnections[connection2].Contains(connection.Key))
-            {
-                var group = string.Join(",", new string[] { connection.Key, connection1, connection2 }.Order());
+        bool allConnected = true;
 
-                if (!options.Contains(group))
-                {
-                    options.Add(group);
-                }
+        foreach (var inGroup in group)
+        {
+            if (!connection.Value.Contains(inGroup))
+            {
+                allConnected = false;
+                break;
             }
+        }
+
+        if (allConnected)
+        {
+            group.Add(connection.Key);
+
+            Console.WriteLine(string.Join(",", group));
         }
     }
 }
 
-var total = options.Count(o => o.StartsWith("t") || o.Contains(",t"));
+var maxGroup = groups.Max(g => g.Count);
+
+var total = string.Join(",", groups.First(g => g.Count == maxGroup).Order());
 
 Console.WriteLine($"Total = {total}");
